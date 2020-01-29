1ac6f48108d2850a5708791e36156e5b536a4b09
Digitech <lukethemodded@gmail.com>
2020-01-29 02:36:35 +0000

    fix for linux
        /* /usr/bin/ld: libLunaLuxEngineLib.a(OpenGL.cpp.o): in function `IRender::initRender()':
         * /home/digitech/Desktop/LunaLuxEngine/src/render/OpenGL.cpp:14: undefined reference to `glewInit'
         * collect2: error: ld returned 1 exit status
         */

--- src/render/OpenGL.cpp
+++ src/render/OpenGL.cpp
@@ -11,14 +11,8 @@
 
 void IRender::initRender()
 {
-    //TODO: Linux platform lib error
-    /* /usr/bin/ld: libLunaLuxEngineLib.a(OpenGL.cpp.o): in function `IRender::initRender()':
-     * /home/digitech/Desktop/LunaLuxEngine/src/render/OpenGL.cpp:14: undefined reference to `glewInit'
-     * collect2: error: ld returned 1 exit status
-     */
-	//GLenum state  = glewInit();
-	//if (state != GLEW_OK)
-		//std::exit(-10);
+    GLenum state  = glewInit();
+    if (state != GLEW_OK) std::exit(-10);
 }
 
 void IRender::destroyRender()
@@ -28,28 +22,28 @@ void IRender::destroyRender()
 
 void IRender::fireRender()
 {
-	for(int8 i = 0; i < entitys.size();i++)
-	{
-		/*glBindVertexArray(block.getMesh().getVao());
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glEnableVertexAttribArray(2);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, block.getMesh().getIbo());
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, block.getMesh().getMaterial().getTextureId());
-		/*shader.bind();
-		shader.setUniform("model", Matrix4f.transform(new Vector3f(0, 0, 0), block.getRotation(), block.getScale()));
-		shader.setUniform("view", Matrix4f.view(camera.getPosition(), camera.getRotation()));
-		shader.setUniform("projection", windowOld.getProjectionMatrix());
-		shader.setUniform("positionOffset", block.getPosition());
-		glDrawElementsInstanced(GL_TRIANGLES, block.getMesh().getIndices().length, GL_UNSIGNED_INT, 0, 1);
-		//shader.unbind();
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-		glDisableVertexAttribArray(2);
-		glBindVertexArray(0);*/
-	}
+    for(int8 i = 0; i < entitys.size();i++)
+    {
+        /*glBindVertexArray(block.getMesh().getVao());
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, block.getMesh().getIbo());
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, block.getMesh().getMaterial().getTextureId());
+        /*shader.bind();
+        shader.setUniform("model", Matrix4f.transform(new Vector3f(0, 0, 0), block.getRotation(), block.getScale()));
+        shader.setUniform("view", Matrix4f.view(camera.getPosition(), camera.getRotation()));
+        shader.setUniform("projection", windowOld.getProjectionMatrix());
+        shader.setUniform("positionOffset", block.getPosition());
+        glDrawElementsInstanced(GL_TRIANGLES, block.getMesh().getIndices().length, GL_UNSIGNED_INT, 0, 1);
+        //shader.unbind();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glDisableVertexAttribArray(2);
+        glBindVertexArray(0);*/
+    }
 }
 
 void IRender::prepRender()
