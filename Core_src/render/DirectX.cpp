60eab26b91d9a0baea5761618fef35bcabcf267a
luke <lukethemodded@gmail.com>
2020-01-30 23:12:02 +0000

    fix

--- Core_src/render/DirectX.cpp
+++ Core_src/render/DirectX.cpp
@@ -3,14 +3,11 @@
 //
 #include "IRender.h"
 #ifdef WIN32
-#ifdef MSVC
-
 #pragma comment ( lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/libs/x64/dxgi.lib" )
 #pragma comment ( lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/libs/x64/d3d11.lib" )
 #pragma comment ( lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/libs/x64/d3d10.lib" )
 #pragma comment ( lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/libs/x64/d3d10_1.lib" )
 #pragma comment ( lib, "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/libs/x64/d3d9.lib" )
-#endif
 #include <d3d11.h>
 #include <d3d9.h>
 #pragma warning( disable : 4996 ) // disable deprecated warning
