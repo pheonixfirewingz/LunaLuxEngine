8691e3b89f9bc47f9241a5babc3a2642b1f52895
luke shore <lukethemodded@gmail.com>
2020-09-08 21:54:33 +0100

    update

--- Includes/CrossWindow/WindowAPI.h
+++ Includes/CrossWindow/WindowAPI.h
@@ -1,6 +1,6 @@
 #ifndef LUNALLUXENGINE_WINDOW_API_H
 #define LUNALLUXENGINE_WINDOW_API_H
 //universal way to import the windows api
-#include "C:/Users/luket/Desktop/LunaLuxEngine Projects/Crosswindow/WindowAPI.h"
+#include "D:/LunaLuxEngine Projects/Crosswindow/WindowAPI.h"
 #endif
 
