3261bda3da243274a3e0630ebe73a582a069011f
luke shore <lukethemodded@gmail.com>
2021-06-29 19:08:52 +0100

    Windows - UtilsScripts: copy built libs so dosn't need to be done manual

---
+++ ClionCopyBuiltLibs.bat
@@ -0,0 +1,5 @@
+copy %CD%\cmake-build-debug\Libs\DirectX12Lib\LunaLuxDirectX12Lib.dll %CD%\cmake-build-debug
+copy %CD%\cmake-build-debug\Libs\NetLib\LunaLuxNetLib.dll %CD%\cmake-build-debug
+copy %CD%\cmake-build-debug\Libs\VulkanLib\LunaLuxVulkanLib.dll %CD%\cmake-build-debug
+copy %CD%\cmake-build-debug\Libs\VulkanUtilLib\LunaLuxVulkanUtilLib.dll %CD%\cmake-build-debug
+copy %CD%\cmake-build-debug\Libs\WindowLib\LunaLuxWindowLib.dll %CD%\cmake-build-debug
